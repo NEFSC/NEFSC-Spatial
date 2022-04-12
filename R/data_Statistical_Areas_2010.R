1ccb45891103547fbf994ab952958a484f6d5669
andybeet <22455149+andybeet@users.noreply.github.com>
2022-04-12 15:25:22 -0400

    created package

---
+++ R/data_Statistical_Areas_2010.R
@@ -0,0 +1,12 @@
+#' Greater Atlantic Region Statistical Areas
+#'
+#' An sf object containing the Greater Atlantic Region Statistical Areas
+#'
+#' @format A data frame/ sf object (135 x2)
+#' \describe{
+#'   \item{}{}
+#'   \item{}{}
+#'   ...
+#' }
+#' @source \url{https://www.fisheries.noaa.gov/resource/map/greater-atlantic-region-statistical-areas}
+"Statistical_Areas_2010"
