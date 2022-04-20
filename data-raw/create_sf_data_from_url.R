5b62259e8326438920a4f62129e77c405aefc142
andybeet <22455149+andybeet@users.noreply.github.com>
2022-04-20 12:18:43 -0400

    strip .shp extension from filename prior to exporting.

--- data-raw/create_sf_data_from_url.R
+++ data-raw/create_sf_data_from_url.R
@@ -1,11 +1,18 @@
 #' Read in shape files from within zip file on web
 #'
+#' Zip file can contain multiple zipfiles. All shapefiles will be converted to sf objects and
+#' copied to data folder
+#'
 #' @param url Character string. Url of zip file containing shapefiles
 #'
 
 library(magrittr)
+library(usethis)
 url <- "https://www.nafo.int/Portals/0/GIS/Divisions.zip"
-
+url <- "https://www.nafo.int/Portals/0/GIS/Footprint_Projected.zip"
+url <- "https://www.nafo.int/Portals/0/GIS/VME_closures.zip"
+#options(warn=-1)
+##options(warn=0)
 
 create_sf_data_from_url <- function(url) {
 
@@ -44,13 +51,16 @@ create_sf_data_from_url <- function(url) {
       for (ifile in 1:nrow(filesInZip)) {
         afile <- filesInZip$Name[ifile]
         if (grepl("shp$",afile)) {
-          ## read in shp file and create sf object
+          ## read in shp file , create sf object, and export to data folder
+          message(paste0("Shapefile = ",afile))
           exportName <- tail(unlist(strsplit(afile,"/")),1)
+          exportName <- head(unlist(strsplit(exportName,"\\.")),1)
           layer <- sf::st_read(dsn=paste0(temp2,"/",afile),quiet=T)
           centroids <-  sf::st_coordinates(sf::st_centroid(layer))
           layer <- cbind(layer,centroids)
           assign(exportName,layer)
-          save(list=exportName,file=paste0(here::here("data-raw"),"/",exportName,".rdata"))
+          do.call("use_data", list(as.name(exportName), overwrite = TRUE))
+          #save(list=exportName,file=paste0(here::here("data-raw"),"/",exportName,".rdata"))
        }
 
       }
@@ -58,6 +68,5 @@ create_sf_data_from_url <- function(url) {
       base::unlink(temp)
       base::unlink(temp2)
 
-      return()
 
 }
