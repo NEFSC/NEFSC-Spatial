b085f71c8854d6fa0fabf8926dfc575f8b99695e
andybeet <22455149+andybeet@users.noreply.github.com>
2022-04-21 11:31:05 -0400

    added documentation for - statistical_areas_2010 - statistical_areas_2010_withNames

--- R/data_Statistical_Areas_2010_withNames.R
+++ R/data_Statistical_Areas_2010_withNames.R
@@ -3,10 +3,15 @@
 #' An sf object containing the Greater Atlantic Region Statistical Areas.
 #' Pulled from ftp://ftp.nefsc.noaa.gov/pub/gis/
 #'
-#' @format A data frame/ sf object (135 x 1)
+#' @format A data frame/ sf object (135 x 6)
 #' \describe{
 #'   \item{Id}{Area Number}
+#'   \item{NAFODIV}{NAFO Division code}
+#'   \item{FULL_NAME}{}
+#'   \item{SHORT_NAME}{Area Number}
+#'   \item{X}{x coordinate of polygon center}
+#'   \item{y}{y coordinate of polygon center}
 #' }
 #'
 #' @source \url{https://www.fisheries.noaa.gov/resource/map/greater-atlantic-region-statistical-areas}
-"Statistical_Areas_2010"
+"Statistical_Areas_2010_withNames"
