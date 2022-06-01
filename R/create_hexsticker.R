cde5c2dc4f9a751e2aea4fd5029e0895e2b6cf8b
kimberly-bastille <kimberly.bastille@gmail.com>
2022-06-01 13:08:06 -0400

    add hexsticker second option

--- R/create_hexsticker.R
+++ R/create_hexsticker.R
@@ -3,15 +3,30 @@ library(dplyr)
 library(hexSticker)
 library(ggimage)
 
-img<- ggimage::image_read2("images/hexstickerv1.PNG")
-res<- img %>% 
+### Option 1
+img1<- ggimage::image_read2("images/hexstickerV1_ppt.PNG")
+res1<- img %>% 
   #image_convert("png") %>% 
   magick::image_resize("1080 x 200")#%>% 
   #magick::image_fill(color="#062047", point="+45") %>% 
   #magick::image_annotate("NEFSCspatial", size=38, location = "-10+100", color="black") 
   
-res_final<- hexSticker::sticker(res, package = "NEFSCspatial", p_size = 12, 
+res_final1<- hexSticker::sticker(res, package = "NEFSCspatial", p_size = 12, 
                                 p_y = 1.4, p_color = "#9DC3E6",
                                 s_x = 1, s_y=1.1, s_width = 1.5, 
                                 s_height = 14, h_fill = "white", h_color="#1F4E79", 
                                 filename = "NEFSCspatial_hexsticker_v1.png")
+
+### Option 2
+img2<- ggimage::image_read2("images/hexstickerV2_ppt.PNG")
+res2<- img2 %>% 
+  #image_convert("png") %>% 
+  magick::image_resize("1080 x 600")#%>% 
+#magick::image_fill(color="#062047", point="+45") %>% 
+#magick::image_annotate("NEFSCspatial", size=38, location = "-10+100", color="black") 
+
+res_final2<- hexSticker::sticker(res2, package = "NEFSCspatial", p_size = 16, 
+                                 p_y = 1.3, p_color = "#1F4E79",
+                                 s_x = 0.95, s_y=1.05, s_width = 1.2, 
+                                 s_height = 12, h_fill = "white", h_color="#1F4E79", 
+                                 filename = "NEFSCspatial_hexsticker_v2.png")
