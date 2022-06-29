8c88ed5d3e47d236d6c06f343378037c27be7931
jmhatch <jmhatch@vt.edu>
2022-06-29 16:38:21 -0400

    add BOEM WEAs documentation

---
+++ R/data_boem_wea_outlines.R
@@ -0,0 +1,19 @@
+#' @title BOEM Renewable Energy Lease Areas and Wind Planning Areas
+#'
+#' @description A \code{sf} object containing the outlines for BOEM Renewable Energy Lease Areas and Wind Planning Areas.
+#'
+#' @format A \code{sf} collection with 696 features and 30 fields:
+#' \describe{
+#'   \item{Geometry type}{GEOMETRY}
+#'   \item{Dimension}{XY}
+#'   \item{Bounding box}{xmin: -158.6636 ymin: 20.77651 xmax: -70.02155 ymax: 43.95971}
+#'   \item{Geodetic CRS}{WGS 84}
+#'   \item{source}{data-raw/boem-renewable-energy-geodatabase/BOEMWindLayers_4Download.gdb}
+#' }
+#' 
+#' @docType data
+#' @name boem_wea_outlines
+#' @usage data("boem_wea_outlines")
+#' @keywords datasets
+#' @source \url{https://www.boem.gov/renewable-energy/mapping-and-data/renewable-energy-gis-data}
+NULL
