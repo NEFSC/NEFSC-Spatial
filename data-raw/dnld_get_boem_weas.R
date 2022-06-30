08c034eca3dbab58607004e1a165b78dd4d9f606
jmhatch <jmhatch@vt.edu>
2022-06-30 13:11:30 -0400

    updated dnld_get_boem_weas.R script

--- data-raw/dnld_get_boem_weas.R
+++ data-raw/dnld_get_boem_weas.R
@@ -1,13 +1,15 @@
 #' Download boundaries of renewable energy lease areas, wind planning areas, and marine hydrokinetic planning areas from BOEM.
 #'
+#' @param gdb_loc File path for the BOEM geodatabase.
+#'
 # needed R libraries
 library(dplyr)
 
+# location to save / access the gdb
+gdb_loc <- 'data-raw/boem-renewable-energy-geodatabase'
+
 # function to download geodatabase (gdb)
-dnld_boem_weas <- function() {
-  
-  # location for gdb
-  gdb_loc <- 'data-raw/boem-renewable-energy-geodatabase'
+dnld_boem_weas <- function(gdb_loc) {
   
   # download gdb
   httr::GET('https://www.boem.gov/BOEM-Renewable-Energy-Geodatabase.zip', 
@@ -30,7 +32,7 @@ dnld_boem_weas <- function() {
 sf::sf_use_s2(FALSE) # turn off s2 processing
 
 # function to extract WEAs
-get_boem_weas <- function(save_clean = TRUE) {
+get_boem_weas <- function(gdb_loc, save_clean = TRUE) {
 
   # list layers in gdb
   list_layers <- here::here(paste0(gdb_loc, '/BOEMWindLayers_4Download.gdb')) %>% sf::st_layers()
@@ -60,7 +62,7 @@ get_boem_weas <- function(save_clean = TRUE) {
 }
 
 # download
-dnld_boem_weas()
+dnld_boem_weas(gdb_loc = gdb_loc)
 
 # extract
-get_boem_weas()
+get_boem_weas(gdb_loc = gdb_loc)
