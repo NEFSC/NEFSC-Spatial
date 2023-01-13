fcce9df3cf20587b0da9c976e8604aaf6525a58b
jmhatch <jmhatch@vt.edu>
2023-01-13 08:25:26 -0500

    Adding requested LMA1 Restricted Area

---
+++ R/data_LMA1_Restricted_Area_FR.R
@@ -0,0 +1,19 @@
+#' @title LMA 1 Restricted Area
+#'
+#' @description An \code{sf} object that depicts the boundaries of the Lobster Management Area One Restricted Area in effect annually from October 1 to January 30 for the NOAA Fisheries Service’s Greater Atlantic Regional Fisheries Office (GARFO).
+#'
+#' @format An \code{sf} object:
+#' \describe{
+#'   \item{Geometry type}{POLYGON}
+#'   \item{Dimension}{XY}
+#'   \item{Bounding box}{xmin: -69.6128 ymin: 42.89198 xmax: -68.28787 ymax: 43.73333}
+#'   \item{Geodetic CRS}{WGS 84}
+#'   \item{Metadata}{See source}
+#' }
+#' 
+#' @docType data
+#' @name LMA1_Restricted_Area_FR
+#' @usage data("LMA1_Restricted_Area_FR")
+#' @keywords datasets
+#' @source \url{https://www.fisheries.noaa.gov/resource/map/lma-1-restricted-area}
+NULL
---
+++ data-raw/get_nmfs_remote_sf.R
@@ -0,0 +1,6 @@
+## Various NOAA Fisheries spatial data
+
+# Download and process spatial data from NOAA Fisheries (NMFS) websites (New England/Mid-Atlantic)
+
+# download, process, and save
+create_sf_data_from_url(url = 'https://media.fisheries.noaa.gov/2022-08/LMA1_Restricted_Area_FR_2.zip')
