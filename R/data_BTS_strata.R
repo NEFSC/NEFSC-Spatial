908c347e1c35a7e35a5f0fc6a3320e66c4a8e2e6
andybeet <22455149+andybeet@users.noreply.github.com>
2023-05-30 11:01:33 -0400

    Documentation for BTS_stratat data set

---
+++ R/data_BTS_strata.R
@@ -0,0 +1,20 @@
+#' @title NEFSC Bottom Trawl Survey Strata
+#'
+#' @description An \code{sf} object containing the boundaries of the NEFSC bottom trawl survey strata
+#'
+#'
+#' @format An \code{sf} object:
+#' \describe{
+#'   \item{Geometry type}{POLYGON}
+#'   \item{dimension}{XY}
+#'   \item{Bounding box}{xmin: -79.18422 ymin: 32.50221 xmax: -65.16869 ymax: 44.83202}
+#'   \item{Geodetic CRS}{NAD 83}
+#'   \item{Metadata}{See source}
+#' }
+#'
+#' @docType data
+#' @name BTS_Strata
+#' @usage BTS_Strata
+#' @keywords datasets
+#' @source \url{https://drive.google.com/drive/folders/15s70lDwezatlNVReDMWRJadtlklMYSnC}
+NULL
