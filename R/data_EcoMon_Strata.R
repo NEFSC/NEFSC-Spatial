497a8bec25ffe361bccff392d0186337b3762173
andybeet <22455149+andybeet@users.noreply.github.com>
2023-05-31 17:07:55 -0400

    Ecomon data

---
+++ R/data_EcoMon_Strata.R
@@ -0,0 +1,16 @@
+#' Ecosystem Monitoring Cruise (EcoMon) strata
+#'
+#' An sf object containing the EcoMon strata
+#'
+#' @format A data frame/ \code{sf} object
+#' \describe{
+#'   \item{Geometry type}{POLYGON}
+#'   \item{Dimension}{XY}
+#'   \item{Bounding box}{xmin: -75.96791 ymin: 35.14219 xmax: -65.16869 ymax: 44.48558}
+#'   \item{Geodetic CRS}{NAD 83}
+#'   \item{Metadata}{See source}
+#'   \item{Features}{47 x 3}
+#' }
+#'
+#' @source \url{https://www.fisheries.noaa.gov/inport/item/33468}
+"EcoMon_Strata"
