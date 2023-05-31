731196d87797f4e83e0f015f7cb2176b8e5d0d03
andybeet <22455149+andybeet@users.noreply.github.com>
2023-05-31 17:11:19 -0400

    added NAFO data documentation

---
+++ R/data_NAFO_Divsions_2021_poly_not_clipped.R
@@ -0,0 +1,20 @@
+#' @title NAFO Divisions (Polygon not clipped to coastline)
+#'
+#' @description An \code{sf} object that depicts the boundaries of the NAFO regions
+#'
+#' @format An \code{sf} object:
+#' \describe{
+#'   \item{Geometry type}{POLYGON}
+#'   \item{Dimension}{XY}
+#'   \item{Bounding box}{xmin: -80 ymin: 35 xmax: -42 ymax: 78.16667}
+#'   \item{Geodetic CRS}{NAD 83}
+#'   \item{Features}{38 x 8}
+#'   \item{Metadata}{See source}
+#' }
+#'
+#' @docType data
+#' @name NAFO_Divisions_2021_poly_not_clipped
+#' @usage data("NAFO_Divisions_2021_poly_not_clipped")
+#' @keywords datasets
+#' @source \url{https://www.nafo.int/Data/GIS}
+NULL
