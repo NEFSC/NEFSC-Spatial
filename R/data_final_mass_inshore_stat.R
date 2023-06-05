fa085772406e2b0f18c0b6f5bceede6682d73dd3
andybeet <22455149+andybeet@users.noreply.github.com>
2023-06-05 14:33:43 -0400

    added documentation for final_mass_inshore_stat - included pkgdown.yml entry

--- R/data_final_mass_inshore_stat.R
+++ R/data_final_mass_inshore_stat.R
@@ -2,16 +2,16 @@
 #'
 #' @description An \code{sf} object containing the boundaries of the Massachusetts Inshore statistical areas.
 #'
-#'
 #' @format An \code{sf} object:
 #' \describe{
 #'   \item{Geometry type}{MULTIPOLYGON}
-#'   \item{dimension}{XY}
+#'   \item{Dimension}{XY}
 #'   \item{Bounding box}{xmin: -71.12554 ymin: 41.18887 xmax: -69.86013 ymax: 42.87647}
 #'   \item{Geodetic CRS}{NAD83}
 #'   \item{Features}{14 x 4}
 #'   \item{Metadata}{See source}
 #' }
+#'
 #'
 #' @docType data
 #' @name final_mass_inshore_stat
