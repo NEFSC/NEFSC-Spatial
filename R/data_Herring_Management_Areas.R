8fd0a24bfaa7d0b0c25c31096bcec0a7699f73eb
andybeet <22455149+andybeet@users.noreply.github.com>
2023-06-06 12:57:51 -0400

    added herring_management_areas data - documentation - pkgdown entry

---
+++ R/data_Herring_Management_Areas.R
@@ -0,0 +1,21 @@
+#' @title Herring Management Areas
+#'
+#' @description An \code{sf} object depicting the boundaries of the Herring Management Areas
+#'
+#' @format An \code{sf} object:
+#' \describe{
+#'   \item{Geometry type}{MULTIPOLYGON}
+#'   \item{Dimension}{XY}
+#'   \item{Bounding box}{xmin: -78.57412 ymin: 33.81288 xmax: -65.69947 ymax: 45.1918}
+#'   \item{Geodetic CRS}{NAD83}
+#'   \item{Features}{4 x 25}
+#'   \item{Metadata}{See source}
+#' }
+#'
+#'
+#' @docType data
+#' @name Herring_Management_Areas
+#' @usage data("Herring_MAnagement_Areas")
+#' @keywords datasets
+#' @source \url{https://www.fisheries.noaa.gov/resource/map/herring-management-areas}
+NULL
