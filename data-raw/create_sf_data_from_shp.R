3e80892fb317bfe7e7a2d038a0051d98f365d375
andybeet <22455149+andybeet@users.noreply.github.com>
2023-07-10 15:20:10 -0400

    revert. wrong branch

--- data-raw/create_sf_data_from_shp.R
+++ data-raw/create_sf_data_from_shp.R
@@ -6,14 +6,12 @@
 #'
 #' @param filePath Character string. Full path to file (eg. here::here("data-raw/gis/shapefile.shp"))
 #' @param overwite Boolean. Export to "data" folder (Default = F)
-#' @param addCentroids Boolean. Calculate and add centroids (X,Y fields) to shapefile
-#' @param shapefilenm Character string. Custom name for sf object. Default = shapefile name
 #'
 #' @return creates sf object in data folder. Package then needs to be rebuilt (Ctrl+shift+B)
 #'
 
 library(magrittr)
-create_sf_data_from_shp <- function(filePath,overwrite=F,addCentroids=T,shapefilenm=NULL) {
+create_sf_data_from_shp <- function(filePath,overwrite=F) {
 
   # turn off spherical geometry
   sf::sf_use_s2(F)
@@ -22,29 +20,20 @@ create_sf_data_from_shp <- function(filePath,overwrite=F,addCentroids=T,shapefil
   fileName <- gsub(".shp$","",filePath)
   fileName <- tail(unlist(strsplit(fileName,"/")),1)
   fn <- fileName
-  #overwrite filename with custom name
-  if(!is.null(shapefilenm)) {
-    fn <- shapefilenm
-  }
-
 
   # read in shapefile
   layer <- sf::st_read(dsn=filePath)
   # add XY locations for center of each polygon
-  if(addCentroids) {
-    centroids <-  sf::st_coordinates(sf::st_centroid(layer))
-    layer <- cbind(layer,centroids)
-  }
+  centroids <-  sf::st_coordinates(sf::st_centroid(layer))
+  layer <- cbind(layer,centroids)
+
   # assigns name to layer object
-    assign(fileName,layer)
-
-
+  assign(fileName,layer)
 
   print(here::here("data",paste0(fn,".rdata")))
   # exports
   if(overwrite) {
     save(list=fileName,file=here::here("data",paste0(fn,".rda")))
-
   }
 
   #do.call(myfun, list(as.name(fileName), overwrite = TRUE))
