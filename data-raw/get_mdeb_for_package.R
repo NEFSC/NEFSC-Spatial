a138b160fbf587911faf85c67167c1b57118d192
jmhatch <jmhatch@vt.edu>
2025-08-05 12:34:51 -0400

    Automate data docs for mdeb datasets

--- data-raw/get_mdeb_for_package.R
+++ data-raw/get_mdeb_for_package.R
@@ -7,6 +7,7 @@
 
 source(here::here("data-raw/find_mdeb_data.R"))
 source(here::here("data-raw/get_mdeb_data.R"))
+source(here::here("data-raw/generate_roxygen_mdeb.R"))
 
 get_mdeb_for_package <- function(overwrite = FALSE) {
   # find all the data set on the MDEB HUB
@@ -29,6 +30,23 @@ get_mdeb_for_package <- function(overwrite = FALSE) {
           file = here::here("data", paste0(fn, ".rda"))
         )
       }
+      # Grab description from metadata (if available)
+      fs_layer <- paste0(dataset$url, '?f=pjson') |>
+        httr2::request() |>
+        httr2::req_perform() |>
+        httr2::resp_body_json()
+      dataset_description <- fs_layer$description
+      # Work around for now, until the descriptions are populated for each layer in the Hub
+      if (dataset_description != '') {
+        modified_dataset_description <- paste0('An \\code{sf} object containing spatial data for the ', 
+                                               dataset$title, '. ',
+                                               dataset_description
+                                               )
+      } else {
+        modified_dataset_description <- paste0('An \\code{sf} object containing spatial data for the ', dataset$title, '.')
+      }
+      # Generate Roxygen data doc
+      generate_roxygen_mdeb(dataset_name = paste0(dataset_name, '.rda'), title = dataset$title, description = modified_dataset_description)
       # save(data, file = here::here("data",))
     } else {
       message("No data found for: ", dataset$name)
