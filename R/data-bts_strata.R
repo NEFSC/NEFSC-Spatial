7fc40a6eb8f29cf81faaf061295d2fb4882284ac
andybeet <22455149+andybeet@users.noreply.github.com>
2026-07-14 15:55:47 -0400

    docs(datasets): add documetation for new data sets

---
+++ R/data-bts_strata.R
@@ -0,0 +1,28 @@
+#' Bottom Trawl Survey
+#'
+#' @description An \code{sf} object containing spatial data for the Bottom Trawl Survey.
+#'
+#' @format An \code{sf} object:
+#' \describe{
+#'   \item{Geometry type}{POLYGON}
+#'   \item{Dimension}{XY}
+#'   \item{Bounding box}{xmin: -76.2 ymin: 34.4 xmax: -65.6 ymax: 44.5}
+#'   \item{Projected CRS}{WGS 84}
+#'   \item{Features}{82 x 15}
+#'   \item{Metadata}{See source}
+#' }
+#'
+#' @docType data
+#' @name bts_strata
+#' @usage data('bts_strata')
+#' @keywords {datasets}
+#' @source {https://mdeb-nefsc-noaa.hub.arcgis.com/}
+#' @examples
+#' \dontrun{
+#' # Load the dataset
+#' data(bts_strata)
+#'
+#' # Plot the geometry
+#' plot(sf::st_geometry(bts_strata))
+#' }
+NULL
---
+++ R/data-ecomon_stations.R
@@ -0,0 +1,28 @@
+#' Ecosystem Monitoring Survey
+#'
+#' @description An \code{sf} object containing spatial data for the Ecosystem Monitoring Survey. There are thirty-five Ecosystem Monitoring fixed hydrographic sampling stations across the Northeast Shelf ecosystem. Four transects run from inshore to off the continental shelf from north of Cape Hatteras to Cape Cod. One transect bisects the Gulf of Maine from the coast of Maine and across Georges Bank. The remaining twelve stations in the Gulf of Maine sample deep basins, banks, coastal areas, and the inflow (Northeast Channel) and out flow (Great South Channel) of the gulf.
+#'
+#' @format An \code{sf} object:
+#' \describe{
+#'   \item{Geometry type}{POINT}
+#'   \item{Dimension}{NA}
+#'   \item{Bounding box}{xmin: -75.5 ymin: 36 xmax: -65.4 ymax: 44.5}
+#'   \item{Projected CRS}{WGS 84}
+#'   \item{Features}{35 x 13}
+#'   \item{Metadata}{See source}
+#' }
+#'
+#' @docType data
+#' @name ecomon_stations
+#' @usage data('ecomon_stations')
+#' @keywords {datasets}
+#' @source {https://mdeb-nefsc-noaa.hub.arcgis.com/}
+#' @examples
+#' \dontrun{
+#' # Load the dataset
+#' data(ecomon_stations)
+#'
+#' # Plot the geometry
+#' plot(sf::st_geometry(ecomon_stations))
+#' }
+NULL
